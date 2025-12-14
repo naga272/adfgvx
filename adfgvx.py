29988f1ee57a94ef350c27fa04f110c4d9f960a2
Nagato272 <114398392+naga272@users.noreply.github.com>
2025-12-14 23:43:11 +0200

    Update adfgvx.py

--- adfgvx.py
+++ adfgvx.py
@@ -1,5 +1,4 @@
 import sys
-import os
 
 
 symbol = ["a", "d", "f", "g", "v", "x"]
@@ -53,35 +52,73 @@ def algoritmo_adfgvx(to_encrypt: str) -> str:
     for idx, c in enumerate(stringa_rimappata):
         lista_prov[idx % len(key_word)].append(c)
 
-    print("stringa rimappata secondo la tabella adfgvx:", stringa_rimappata)
-    print("keyword riordinata:", keyword_riordinata)
+    # questo serve solo per l'output
+    print(
+        "stringa rimappata secondo la tabella adfgvx:\n",
+        stringa_rimappata,
+        end="\n\n"
+    )
+    print("keyword originale:")
+    print("".join(char + "  " for char in key_word))
+    print("".join(f"{ord(char)} " for char in key_word))
+
+    print(keyword_riordinata)
+
+    # ristampa la nuova matrice
+    len_key = len(key_word)
+
+    # la stringa viene formattata
+    # [a, b, c, d, e]
+    print("[", end="")
+    for idx, char in enumerate(stringa_rimappata):
+        chiusura = (idx + 1) % len_key != 0
+        print(char, end=", " if chiusura else "")
+
+        if not chiusura:
+            print("]")
+            if idx + 1 != len(stringa_rimappata):
+                print("[", end="")
 
     string_encrypted = ""
-    for idx in keyword_riordinata:
-        string_encrypted += "".join(char for char in lista_prov[idx])
+    sections = []
 
-    return string_encrypted
+    for i in range(len_key):
+        sections.append("")
+
+    for idx, char in enumerate(stringa_rimappata):
+        sections[idx % len_key] += char
+
+    for section in sections:
+        print(section)
+
+    sorted_indices = sorted(
+        range(
+            len(keyword_riordinata)
+        ),
+        key=lambda i: keyword_riordinata[i]
+    )
+    for num in sorted_indices:
+        string_encrypted += sections[num] + " "
+
+    return string_encrypted.upper()
 
 
 def main():
-    stringa = "lorem"
+    stringa = "MunitionierungbeschleunigenPunktSoweitnichteingesehenauchbeiTag".lower()
 
-    print("stringa da crittografare: ", stringa)
-
+    print("stringa da crittografare:\n", stringa.upper())
     print("matrice adfgvx: ")
     print(" ", symbol)
+
     for idx, lista in enumerate(matrice_adfgvx):
         print(symbol[idx], lista)
 
-    print("parola chiave: ", key_word)
-
     testo_crittografato = algoritmo_adfgvx(stringa)
 
-    print("stringa crittografata: ", testo_crittografato)
+    print("stringa crittografata:\n" + testo_crittografato)
     return 0
 
 
 if __name__ == "__main__":
     result = main()
     sys.exit(result)
-
