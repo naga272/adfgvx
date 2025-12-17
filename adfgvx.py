98cdfdd0cd9150e1bf6161559836a6812bc84046
naga272 <bastianellofederico4@gmail.com>
2025-12-17 05:21:56 +0100

    piccoli fix e riordinamento del codice

--- adfgvx.py
+++ adfgvx.py
@@ -1,5 +1,4 @@
 import sys
-import os
 
 
 symbol = ["a", "d", "f", "g", "v", "x"]
@@ -33,7 +32,60 @@ def re_map(string_to_remap: str) -> str:
 
 
 def order(key: str) -> list:
-    return sorted(range(len(key)), key=lambda i: key[i])
+    test = sorted(range(len(key)), key=lambda i: key[i])
+    print("vedi test", test)
+    return test
+
+
+def trasposizione(stringa_rimappata: str) -> str:
+    keyword_riordinata = order(key_word)
+
+    # questo serve solo per l'output
+    print(
+        "stringa rimappata secondo la tabella adfgvx:\n" +
+        stringa_rimappata,
+        end="\n\n"
+    )
+    print(
+        "keyword originale:\n" +
+        "".join(char + "  " for char in key_word) +
+        "\n" +
+        "".join(f"{ord(char)} " for char in key_word) +
+        "\n" +
+        f"{keyword_riordinata}"
+    )
+
+    # ristampa la nuova matrice
+    len_key = len(key_word)
+
+    # la stringa viene formattata
+    # [a, b, c, d, e]
+    print("[", end="")
+    for idx, char in enumerate(stringa_rimappata):
+        chiusura = (idx + 1) % len_key != 0
+        print(char, end=", " if chiusura else "")
+
+        if not chiusura:
+            print("]")
+            if idx + 1 != len(stringa_rimappata):
+                print("[", end="")
+
+    string_encrypted = ""
+    sections = []
+
+    for i in range(len_key):
+        sections.append("")
+
+    for idx, char in enumerate(stringa_rimappata):
+        sections[idx % len_key] += char
+
+    for section in sections:
+        print(section)
+
+    for idx in keyword_riordinata:
+        string_encrypted += sections[idx] + " "
+
+    return string_encrypted.upper()
 
 
 def algoritmo_adfgvx(to_encrypt: str) -> str:
@@ -44,44 +96,26 @@ def algoritmo_adfgvx(to_encrypt: str) -> str:
     stringa_rimappata = re_map(to_encrypt)
 
     # step 2: trasposizione colonne tramite keyword
-    keyword_riordinata = order(key_word)
-
-    lista_prov = []
-    for i in range(len(key_word)):
-        lista_prov.append([])
-
-    for idx, c in enumerate(stringa_rimappata):
-        lista_prov[idx % len(key_word)].append(c)
-
-    print("stringa rimappata secondo la tabella adfgvx:", stringa_rimappata)
-    print("keyword riordinata:", keyword_riordinata)
-
-    string_encrypted = ""
-    for idx in keyword_riordinata:
-        string_encrypted += "".join(char for char in lista_prov[idx])
-
-    return string_encrypted
+    stringa_finale = trasposizione(stringa_rimappata)
+    return stringa_finale
 
 
 def main():
-    stringa = "lorem"
+    stringa = "MunitionierungbeschleunigenPunktSoweitnichteingesehenauchbeiTag".lower()
 
-    print("stringa da crittografare: ", stringa)
-
+    print("stringa da crittografare:\n", stringa.upper())
     print("matrice adfgvx: ")
     print(" ", symbol)
+
     for idx, lista in enumerate(matrice_adfgvx):
         print(symbol[idx], lista)
 
-    print("parola chiave: ", key_word)
-
     testo_crittografato = algoritmo_adfgvx(stringa)
 
-    print("stringa crittografata: ", testo_crittografato)
+    print("stringa crittografata:\n" + testo_crittografato)
     return 0
 
 
 if __name__ == "__main__":
     result = main()
     sys.exit(result)
-
